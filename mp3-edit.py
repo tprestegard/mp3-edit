330a6034a4a6b0b2a80fedf6c01fadfc0e7f002f
Tanner Prestegard <tanner.prestegard@ligo.org>
2016-11-08 09:11:15 -0600

    adding ideas

--- mp3-edit.py
+++ mp3-edit.py
@@ -14,10 +14,13 @@ import pdb
 
 # Options:
 #  Add artwork
-#  Edit tags
+#  Edit tags - example: get all artist tags from songs in album, pick one and apply to all.
 #  Edit filenames
 #  Clean up bad tags
 #  Do any of these for a single directory (album) or an entire artist's collection, or multiple artists.
 #    Note: might be tricky to handle spaces in filenames.
 #    Note: handle the cases where an album has multiple discs.
 #  Create classes for Artist and Album objects, Albums can be associated with Artists, etc.
+#  Album art idea: pull from discogs.com if it doesn't exist in folder?  Could be an optional argument.  Will have to use string similarity comparators, probably, since there may be differences in album names (1 vs. one, etc.).
+#  Write logs to ~/.mp3-edit-logs if --log.
+#  Should this have a command-line interface or just be a Python library?
