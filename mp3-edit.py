9ffca80808934262c9c2e2d6fb68039d9e5af210
Tanner Prestegard <tanner.prestegard@ligo.org>
2016-11-08 09:07:34 -0600

    initial commit

---
+++ mp3-edit.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+# Imports
+import pytaglib
+import argparse
+import Subprocess
+import pdb
+
+
+# Check for presence of system packages
+# (lame, mp3info)
+
+# Parse arguments
+
+# Options:
+#  Add artwork
+#  Edit tags
+#  Edit filenames
+#  Clean up bad tags
+#  Do any of these for a single directory (album) or an entire artist's collection, or multiple artists.
+#    Note: might be tricky to handle spaces in filenames.
+#    Note: handle the cases where an album has multiple discs.
+#  Create classes for Artist and Album objects, Albums can be associated with Artists, etc.
